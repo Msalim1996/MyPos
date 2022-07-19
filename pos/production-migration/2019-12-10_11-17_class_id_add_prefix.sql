0e66b256620a36d3835a4f37b1d6fdc0ac6105e1
Msalim1996 <59538847+Msalim1996@users.noreply.github.com>
2022-07-19 21:22:09 +0700

    Delete pos directory

--- pos/production-migration/2019-12-10_11-17_class_id_add_prefix.sql
+++
@@ -1,7 +0,0 @@
--- This migration adds prefix for class_id
-
-INSERT INTO db_number_counters
-values(7,'C',NULL,NULL,NULL,1,NOW(),NOW());
-
-INSERT INTO db_number_counters
-values(8,'TMPS',NULL,NULL,NULL,1,NOW(),NOW());
--- pos/routes/public.api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Route::fallback(function () {
-    return response()->json(['message' => 'Report tidak ditemukan!'], 404);
-});
-
-Route::get('/pb1.json', 'ReportJson\PB1JsonController@index');
-Route::get('/locations', 'PublicAPI\PublicLocationController@index');
